96ea915586e068667daa23646cbbc50a0f271214
Noam Zaks <noamzaks@outlook.com>
2020-10-24 19:44:54 +0300

    added signup option (https://learndjango.com/tutorials/django-signup-tutorial)

--- djoweme/djoweme/urls.py
+++ djoweme/djoweme/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from oweme.views import home
+from oweme.views import home, SignUpView
 
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup', SignUpView.as_view())
 ]
